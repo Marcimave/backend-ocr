56fc13147411fbcb1452bf205a11e6cfe9d1a03e
Marcimave <marcimavegithub@gmail.com>
2026-05-26 01:36:37 +0100

    fix: remove duplicate PORT

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ import Med from "./models/Med.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on", PORT);
-});
 
 /**
  * 🌐 ROUTES TEST
